4b8757c4ae23d6a8f99444d6a52e619cccf5d196
Arnab Dutta <arnabdutta8584@gmail.com>
2026-09-16 19:56:49 +0530

    feat: add CORS support and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import taskRoutes from "./task/task.routes.ts";
 
 const app = express();
 
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
